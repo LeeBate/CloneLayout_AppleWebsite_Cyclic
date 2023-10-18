378c675478c177fc03e469f2a37eb7090d2da7c6
phanumat44 <gth361@gmail.com>
2023-10-18 13:52:42 +0700

    update

--- Server.js
+++ Server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const core = require('cors');
 const port = process.env.PORT || 3000; // Use the default port 3000 or use a custom one
 
+// Enable CORS for all requests
+app.use(core());
 // Serve static files (HTML, CSS, JavaScript) from the 'public' directory
 app.use(express.static('public'));
 
